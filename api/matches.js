d0da7cff4ca45f78857629402bf56419cd7f24e1
evanodhino <evanchiodin34@gmail.com>
2026-04-13 18:43:06 +0200

    Update matches.js

--- api/matches.js
+++ api/matches.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   
-  const today = '2026-04-18';
+  const today = '2024-04-20';
   const leagueIds = [61, 39, 140, 78, 135, 2, 3];
   const API_KEY = '1198041d6af647866b261840e19aead8';
   const allMatches = [];
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   for (const league of leagueIds) {
     try {
       const response = await fetch(
-        `https://v3.football.api-sports.io/fixtures?date=${today}&league=${league}&season=2026`,
+        `https://v3.football.api-sports.io/fixtures?date=${today}&league=${league}&season=2024`,
         {
           headers: { 'x-apisports-key': API_KEY }
         }
